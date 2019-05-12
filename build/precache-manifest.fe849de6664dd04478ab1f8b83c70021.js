b39b313616154a3cb451a8f145c48b1c52cb5e45
Elena Frolova <elenschip777@gmail.com>
2019-05-12 23:45:49 +0300

    with build

---
+++ build/precache-manifest.fe849de6664dd04478ab1f8b83c70021.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6c6c7ebc414ee34200eb8e468d35e6",
+    "url": "/goit-react-hw-03-books/build/index.html"
+  },
+  {
+    "revision": "3ccf97139c32b4befb19",
+    "url": "/goit-react-hw-03-books/build/static/css/main.ede6423d.chunk.css"
+  },
+  {
+    "revision": "9efd1f7b1d76d5e56628",
+    "url": "/goit-react-hw-03-books/build/static/js/2.eb3f9da6.chunk.js"
+  },
+  {
+    "revision": "3ccf97139c32b4befb19",
+    "url": "/goit-react-hw-03-books/build/static/js/main.15b8cc02.chunk.js"
+  },
+  {
+    "revision": "7d7a0abc3cd5ec06e53b",
+    "url": "/goit-react-hw-03-books/build/static/js/runtime~main.570f3f94.js"
+  }
+]);
